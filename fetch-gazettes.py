7a56123039f4a84092061ffdc4c91914c615127f
Greg Kempe <greg@kempe.net>
2017-02-13 13:26:23 +0200

    Initial commit

---
+++ fetch-gazettes.py
@@ -0,0 +1,48 @@
+#!/bin/env python
+#
+# Script to generate Registry.txt and pdf-urls.txt for gazettes from opengazettes.org.za
+import json
+import os
+import os.path
+
+entries = []
+urls = []
+count = 0
+
+
+# what jurisdiction code should we use?
+with open("jurisdiction.txt") as f:
+    jurisdiction = f.read().strip()
+print "Only using gazettes for jurisdiction code: %s" % jurisdiction
+
+
+# generate an entry for each gazette entry
+with open("gazette-index-latest.jsonlines", "r") as f:
+    for line in f:
+        gazette = json.loads(line)
+        if gazette['jurisdiction_code'] != jurisdiction:
+            continue
+
+        fname = os.path.basename(gazette['archive_url'])
+        parts = [fname, gazette['publication_date'], gazette['issue_title']]
+        entries.append("|".join(parts))
+        urls.append(gazette['archive_url'])
+
+        count += 1
+        if count == 10:
+            break
+
+
+# write the new registry
+with open("Registry.txt.new", "w") as f:
+    f.write("\n".join(entries))
+
+
+# write the new urls
+with open("pdf-urls.txt.new", "w") as f:
+    f.write("\n".join(urls))
+
+
+# atomically rename the files
+os.rename("Registry.txt.new", "Registry.txt")
+os.rename("pdf-urls.txt.new", "pdf-urls.txt")
---
+++ fetch-gazettes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 
+# Script to echo gazettes from opengazettes.org.za into SAFLII.
+#
+# Uses the full JSON listing from http://code4sa-gazettes.s3.amazonaws.com/archive/index/gazette-index-latest.jsonlines
+# to generate a registry.txt file and a pdf-urls.txt file.
+# Then downloads files in pdf-urls.txt that aren't already on the disk.
+set -e
+
+# get the latest master list
+echo "Fetching gazette list"
+rm -f gazette-index-latest.jsonlines
+wget --no-verbose -a pdf-download.log http://code4sa-gazettes.s3.amazonaws.com/archive/index/gazette-index-latest.jsonlines
+
+# generate the Registry.txt and pdf-urls.txt files
+echo "Generating Registry.txt"
+python fetch-gazettes.py
+
+# get the PDFs
+echo "Downloading PDFs"
+wget --no-verbose --no-clobber -i pdf-urls.txt -P Data -a pdf-download.log
+
+echo "Done, bye."
